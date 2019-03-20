fb1776685910f28d1784bac1632c33cf8dadeb5d
beginor <beginor@qq.com>
2019-03-20 17:37:19 +0800

    add server stuff.

---
+++ server/src/NetCoreApp.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCoreApp.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ server/src/NetCoreApp.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCoreApp.Services
+{
+    public class Class1
+    {
+    }
+}
