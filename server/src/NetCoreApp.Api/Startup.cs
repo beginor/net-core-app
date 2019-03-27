0b75fa428ce571df71696a46a22b8a9fbe428094
beginor <beginor@qq.com>
2019-03-27 15:40:59 +0800

    format

--- server/src/NetCoreApp.Api/Startup.cs
+++ server/src/NetCoreApp.Api/Startup.cs
@@ -23,11 +23,13 @@ namespace Beginor.NetCoreApp.Api {
 
         // This method gets called by the runtime. Use this method to configure
         // the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env
+        ) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
-            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
