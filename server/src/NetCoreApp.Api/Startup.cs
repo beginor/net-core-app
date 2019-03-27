bfd9cf4fd135d70e15c0b275526c0692d5b28b83
beginor <beginor@qq.com>
2019-03-27 15:38:25 +0800

    format.

--- server/src/NetCoreApp.Api/Startup.cs
+++ server/src/NetCoreApp.Api/Startup.cs
@@ -14,12 +14,15 @@ namespace Beginor.NetCoreApp.Api {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add
+        // services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure
+        // the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
