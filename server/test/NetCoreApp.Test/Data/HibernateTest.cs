31106df29fae96deb84b0f9f0678a77df2ced8c5
beginor <beginor@qq.com>
2019-04-17 17:51:01 +0800

    ensure test pass.

--- server/test/NetCoreApp.Test/Data/HibernateTest.cs
+++ server/test/NetCoreApp.Test/Data/HibernateTest.cs
@@ -1,14 +1,26 @@
+using System;
+using System.Linq;
+using Beginor.NetCoreApp.Data.Entities;
 using NHibernate;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Beginor.NetCoreApp.Test.Data {
 
     [TestClass]
-    public class HibernateTest : BaseTest<ISession> {
+    public class HibernateTest : BaseTest<ISessionFactory> {
 
         [TestMethod]
         public void _01_CanResolveTarget() {
             Assert.IsNotNull(Target);
+        }
+
+        [TestMethod]
+        public void _02_CanQueryApplicationUsers() {
+            using (var session = Target.OpenSession()) {
+                var users = session.Query<ApplicationUser>().ToList();
+                Console.WriteLine(users.Count);
+                Assert.IsTrue(users.Count >= 0);
+            }
         }
 
     }
