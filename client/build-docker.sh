e7222ccbcdb09df294de9d100b9527e51ed54948
beginor <beginor@qq.com>
2019-08-07 13:51:52 +0800

    update image build script.

--- client/build-docker.sh
+++ client/build-docker.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-set -e
-
+#!/bin/bash -e
+# Build client, if you want to change what to build, please modify the `build`
+# script in package.json
 npm run build
+# Gzip static files (*.js, *.css).
 find dist -name "*.js" -print0 | xargs -0 gzip -k
 find dist -name "*.css" -print0 | xargs -0 gzip -k
-
-docker build --no-cache --rm -t beginor/net-core-app .
+# Build client image (nginx).
+docker build --no-cache --rm -t beginor/net-core-app/nginx .
