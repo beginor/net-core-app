abd8b6ae257b2b670ba18d097a49eb2b3ca813cf
beginor <beginor@qq.com>
2019-08-09 16:27:32 +0800

    fix sed

--- docker/server/build-api-image.sh
+++ docker/server/build-api-image.sh
@@ -8,8 +8,8 @@ mv bin/Release/netcoreapp2.2/publish/ ../../../docker/server/dist
 rm -rf bin
 cd ../../../docker/server
 # modify config file to run in stagging server;
-sed -i .bak "s/ref=\"ConsoleAppender\"/ref=\"RollingFileAppender\"/g" dist/log.config
-sed -i .bak "s/127\.0\.0\.1/postgis/g" dist/hibernate.config
+sed -i.bak "s/ref=\"ConsoleAppender\"/ref=\"RollingFileAppender\"/g" dist/log.config
+sed -i.bak "s/127\.0\.0\.1/postgis/g" dist/hibernate.config
 rm dist/*.bak
 # Build docker image
 docker build --no-cache --rm -t beginor/net-core-app/api .
--- rename.sh
+++ rename.sh
@@ -9,68 +9,68 @@ CONTEXT_ROOT=tpl-app
 # 新的服务端项目前缀（公司名称+项目名称, 特殊符号需要用 \ 进行转义）
 SERVER_PREFIX="${COMPANY_NAME}.${PROJ_NAME}"
 # 修改 Docker 编译/部署文件
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/build-nginx-image.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/build-nginx-image.sh
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/default.conf
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/deploy-nginx-image.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/deploy-nginx-image.sh
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/Dockerfile
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/Dockerfile
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/build-nginx-image.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/build-nginx-image.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/default.conf
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/deploy-nginx-image.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/deploy-nginx-image.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/client/Dockerfile
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/client/Dockerfile
 
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/build-api-image.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/build-api-image.sh
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./docker/server/build-api-image.sh
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/deploy-api-image.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/deploy-api-image.sh
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/Dockerfile
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/Dockerfile
-sed -i .bak "s/Beginor/${COMPANY_NAME}/g" ./docker/server/Dockerfile
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./docker/server/Dockerfile
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/build-api-image.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/build-api-image.sh
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./docker/server/build-api-image.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/deploy-api-image.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/deploy-api-image.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/server/Dockerfile
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/server/Dockerfile
+sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./docker/server/Dockerfile
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./docker/server/Dockerfile
 
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/build-docker-images.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/build-docker-images.sh
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/docker-compose.yml
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/docker-compose.yml
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/deploy-images.sh
-sed -i .bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/deploy-images.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/build-docker-images.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/build-docker-images.sh
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/docker-compose.yml
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/docker-compose.yml
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./docker/deploy-images.sh
+sed -i.bak "s/beginor/$(echo ${COMPANY_NAME} | tr '[:upper:]' '[:lower:]')/g" ./docker/deploy-images.sh
 # 删除备份文件
 find . -name '*.bak' -delete
 # 提交一下 Docker 文件
 git add *
 git commit -m "Rename docker files"
 # 修改客户端相关文件
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/e2e/src/app.e2e-spec.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/e2e/src/home/home.e2e-spec.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/e2e/src/app.e2e-spec.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/e2e/src/home/home.e2e-spec.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/package-lock.json
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/package.json
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/src/environments/environment.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/src/environments/environment.prod.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/src/environments/environment.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/src/environments/environment.prod.ts
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/proxy.conf.json
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/e2e/src/app.e2e-spec.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/e2e/src/home/home.e2e-spec.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/e2e/src/app.e2e-spec.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/e2e/src/home/home.e2e-spec.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/package-lock.json
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/package.json
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/src/environments/environment.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/src/environments/environment.prod.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/src/environments/environment.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/src/environments/environment.prod.ts
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/proxy.conf.json
 # 删除备份文件
 find . -name '*.bak' -delete
 # 提交一下客户端文件
 git add *
 git commit -m "Rename client app"
 # 修改服务端相关文件
-sed -i .bak "s/net-core-app/${CONTEXT_ROOT}/g" ./server/src/NetCoreApp.Api/Properties/launchSettings.json
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Api/Properties/launchSettings.json
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Api/Startup.Swagger.cs
-grep Beginor.NetCoreApp -rl server --include *.cs | xargs sed -i .bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-grep Beginor.NetCoreApp -rl server --include *.hbm.xml | xargs sed -i .bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i .bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-grep Beginor.NetCoreApp -rl server --include *.csproj | xargs sed -i .bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i .bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/test/NetCoreApp.Test/NetCoreApp.Test.csproj
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./.gitlab-ci.yml
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/launch.json
-sed -i .bak "s/Beginor/${COMPANY_NAME}/g" ./.vscode/launch.json
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/tasks.json
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/smartcode.yml
-sed -i .bak "s/Beginor/${COMPANY_NAME}/g" ./server/smartcode.yml
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./server/src/NetCoreApp.Api/Properties/launchSettings.json
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Api/Properties/launchSettings.json
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Api/Startup.Swagger.cs
+grep Beginor.NetCoreApp -rl server --include *.cs | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
+grep Beginor.NetCoreApp -rl server --include *.hbm.xml | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
+grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
+grep Beginor.NetCoreApp -rl server --include *.csproj | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
+grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/test/NetCoreApp.Test/NetCoreApp.Test.csproj
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.gitlab-ci.yml
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/launch.json
+sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./.vscode/launch.json
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/tasks.json
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/smartcode.yml
+sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./server/smartcode.yml
 # 删除备份文件
 find . -name '*.bak' -delete
 # 提交一下服务端文件
@@ -92,8 +92,8 @@ git mv server/NetCoreApp.sln server/${PROJ_NAME}.sln
 git add *
 git commit -m "Move server app"
 # 修改项目引用路径
-sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g" server/${PROJ_NAME}.sln
-grep NetCoreApp -rl server --include *.csproj | xargs sed -i .bak "s/NetCoreApp/${PROJ_NAME}/g"
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" server/${PROJ_NAME}.sln
+grep NetCoreApp -rl server --include *.csproj | xargs sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g"
 # 删除备份文件
 find . -name '*.bak' -delete
 # 提交一下服务端文件
