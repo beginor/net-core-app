fface5d81a15088c0c961baae730b3ef841f0f72
beginor <beginor@qq.com>
2019-08-22 15:18:37 +0800

    fix lint

--- client/projects/web/src/app/app.module.ts
+++ client/projects/web/src/app/app.module.ts
@@ -36,7 +36,7 @@ import { environment } from '../environments/environment';
         {
             provide: 'apiRoot',
             useFactory: () => {
-                return environment.apiRoot;;
+                return environment.apiRoot;
             }
         }
     ],
