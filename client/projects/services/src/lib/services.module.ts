c13723ee16c5fe396daeaaf37809a06d2381a33b
beginor <beginor@qq.com>
2019-08-29 14:08:57 +0800

    add shared library

---
+++ client/projects/services/src/lib/services.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        HttpClientModule
+    ],
+    exports: []
+})
+export class ServicesModule { }
---
+++ client/projects/services/src/public-api.ts
@@ -0,0 +1,4 @@
+/*
+ * Public API Surface of services
+ */
+export * from './lib/services.module';
