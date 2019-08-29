965516b77066da8461a86282dc1f823dfd14b7b3
beginor <beginor@qq.com>
2019-08-29 14:15:16 +0800

    move common services to shared library.

--- client/projects/services/src/public-api.ts
+++ client/projects/services/src/public-api.ts
@@ -2,3 +2,7 @@
  * Public API Surface of services
  */
 export * from './lib/services.module';
+export * from './lib/xsrf.guard';
+export * from './lib/auth.guard';
+export * from './lib/api.interceptor';
+export * from './lib/account.service';
