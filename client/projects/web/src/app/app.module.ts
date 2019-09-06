338219a8c8bebceb86aca84254f62146c127203c
beginor <beginor@qq.com>
2019-09-06 14:47:59 +0800

    use zh-Hans as locale_id

--- client/projects/web/src/app/app.module.ts
+++ client/projects/web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,6 +32,10 @@ import { environment } from '../environments/environment';
             provide: HTTP_INTERCEPTORS,
             useClass: ApiInterceptor,
             multi: true
+        },
+        {
+            provide: LOCALE_ID,
+            useValue: 'zh-Hans'
         },
         {
             provide: 'apiRoot',
