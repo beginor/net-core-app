d52c44f69568cefdcc362593b7a08a99f1b6c489
beginor <beginor@qq.com>
2019-10-15 13:52:10 +0800

    update docker script.

--- docker/server/build-api-image.sh
+++ docker/server/build-api-image.sh
@@ -3,6 +3,7 @@
 dotnet publish -c Release -o dist ../../server/src/NetCoreApp.Api/NetCoreApp.Api.csproj
 # modify config file to run in stagging server;
 sed -i.bak "s/ref=\"ConsoleAppender\"/ref=\"RollingFileAppender\"/g" dist/log.config
+sed -i.bak "s/DEBUG/ERROR/g" dist/log.config
 sed -i.bak "s/127\.0\.0\.1/postgis/g" dist/hibernate.config
 rm dist/*.bak
 # Build docker image
