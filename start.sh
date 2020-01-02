c75046bddd8345fe0678730a46fd227aaff6e27d
beginor <beginor@qq.com>
2020-01-03 07:09:13 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -e
-concurrently "cd client && npm run start" "cd server/src/NetCoreApp.Api && dotnet watch run"
+if ! [ -x "$(command -v concurrently)" ]; then
+  echo 'Warning: concurrently is not installed, install it with npm now .' >&2
+  npm i -g concurrently
+fi
+
+concurrently "cd client && npm run start" \
+  "cd server/src/NetCoreApp.Api && dotnet watch run"
