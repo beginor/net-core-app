05f889c2bd83b746df378c21c6439b0352aab80d
beginor <beginor@qq.com>
2020-05-26 12:32:33 +0800

    regenerate model, repo test.

--- server/src/NetCoreApp.Models/AppClientErrorModel.cs
+++ server/src/NetCoreApp.Models/AppClientErrorModel.cs
@@ -4,26 +4,25 @@ using Beginor.AppFx.Core;
 
 namespace Beginor.NetCoreApp.Models {
 
-    /// <summary>客户端错误模型</summary>
-    public class AppClientErrorModel : StringEntity {
+    /// <summary>程序客户端错误记录模型</summary>
+    public partial class AppClientErrorModel : StringEntity {
 
         /// <summary>用户名</summary>
-        public virtual string UserName { get; set; }
-
+        public string UserName { get; set; }
         /// <summary>错误发生时间</summary>
-        public virtual DateTime OccuredAt { get; set; }
-
+        [Required(ErrorMessage = "错误发生时间 必须填写！")]
+        public DateTime OccuredAt { get; set; }
         /// <summary>用户浏览器代理</summary>
-        public virtual string UserAgent { get; set; }
-
+        [Required(ErrorMessage = "用户浏览器代理 必须填写！")]
+        public string UserAgent { get; set; }
         /// <summary>错误发生的路径地址</summary>
-        public virtual string Path { get; set; }
-
+        public string Path { get; set; }
         /// <summary>错误消息</summary>
-        public virtual string Message { get; set; }
+        public string Message { get; set; }
+
     }
 
-    /// <summary>客户端错误搜索模型</summary>
+    /// <summary>程序客户端错误记录搜索参数</summary>
     public partial class AppClientErrorSearchModel : PaginatedRequestModel { }
 
 }
