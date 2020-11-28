e2f0ef7dea4ec330169002a7a20b5a3f36881303
beginor <beginor@qq.com>
2020-11-29 02:04:59 +0800

    fix icon load service.

--- client/projects/app-shared/src/lib/svg-icon.service.ts
+++ client/projects/app-shared/src/lib/svg-icon.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
+
 @Injectable({ providedIn: 'root' })
 export class SvgIconService {
 
     private basePath = 'assets/icons';
-    private iconMap = new Map<string, string>();
+    private loadingMap = new Map<string, Observable<string>>();
 
     constructor(private http: HttpClient) {
     }
@@ -14,19 +17,40 @@ export class SvgIconService {
         this.basePath = basePath;
     }
 
-    public async loadSvgFile(path: string): Promise<string> {
-        if (this.iconMap.has(path)) {
-            return this.iconMap.get(path) as string;
+    public loadSvgFile(path: string): Promise<string> {
+        const key = `svgIcon:${path}`;
+        const loadedItem = sessionStorage.getItem(key);
+        if (!!loadedItem) {
+            return Promise.resolve(loadedItem);
         }
-        const url = `${this.basePath}/${path}.svg`;
-        const svg = await this.http.get(
-            url, { responseType: 'text' }
-        ).toPromise();
-        if (svg.startsWith('<svg')) {
-            this.iconMap.set(path, svg);
-            return svg;
+        if (this.loadingMap.has(key)) {
+            const op = this.loadingMap.get(key) as Observable<string>;
+            return new Promise<string>((resolve, reject) => {
+                op.subscribe({
+                    next: val => resolve(val),
+                    error: ex => reject(ex)
+                });
+            });
         }
-        return '';
+        const loadingOp = this.http.get(
+            `${this.basePath}/${path}.svg`,
+            { responseType: 'text' }
+        ).pipe(share());
+        this.loadingMap.set(key, loadingOp);
+        return new Promise<string>((resolve, reject) => {
+            loadingOp.subscribe({
+                next: val => {
+                    sessionStorage.setItem(key, val);
+                    resolve(val);
+                },
+                error: ex => {
+                    reject(ex);
+                },
+                complete: () => {
+                    this.loadingMap.delete(key);
+                }
+            });
+        });
     }
 
 }
