9570353d282ddbc32a06ce00f18aee003ee8bc52
beginor <beginor@qq.com>
2020-12-05 19:16:55 +0800

    update startup.

--- server/src/NetCoreApp.Entry/Startup.App.cs
+++ server/src/NetCoreApp.Entry/Startup.App.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Reflection;
 using Beginor.AppFx.DependencyInjection;
@@ -6,7 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Beginor.NetCoreApp.Entry {
+namespace Beginor.GisHub.Entry {
 
     partial class Startup {
 
@@ -17,6 +18,23 @@ namespace Beginor.NetCoreApp.Entry {
             var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             services.AddServiceWithDefaultImplements(
                 typeof(Beginor.NetCoreApp.Data.ModelMapping).Assembly,
+                t => t.Name.EndsWith("Repository"),
+                ServiceLifetime.Scoped
+            );
+            services.AddServiceWithDefaultImplements(
+                typeof(Beginor.GisHub.Slpk.ModelMapping).Assembly,
+                t => t.Name.EndsWith("Repository"),
+                ServiceLifetime.Scoped
+            );
+            services.AddSingleton<ConcurrentDictionary<long, Beginor.GisHub.Slpk.Data.SlpkCacheItem>>();
+            services.AddServiceWithDefaultImplements(
+                typeof(Beginor.GisHub.TileMap.ModelMapping).Assembly,
+                t => t.Name.EndsWith("Repository"),
+                ServiceLifetime.Scoped
+            );
+            services.AddSingleton<ConcurrentDictionary<long, Beginor.GisHub.TileMap.Data.TileMapCacheItem>>();
+            services.AddServiceWithDefaultImplements(
+                typeof(Beginor.GisHub.DataServices.ModelMapping).Assembly,
                 t => t.Name.EndsWith("Repository"),
                 ServiceLifetime.Scoped
             );
