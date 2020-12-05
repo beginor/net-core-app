51ba569fa31fb7cb5a9fecbc5674df97163f7506
beginor <beginor@qq.com>
2020-12-05 11:49:20 +0800

    refact svg component

--- client/projects/app-shared/src/lib/svg-icon/svg-icon.component.ts
+++ client/projects/app-shared/src/lib/svg-icon/svg-icon.component.ts
@@ -5,15 +5,13 @@ import { SvgIconService } from './svg-icon.service';
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'svg-icon',
-    template: ``,
-    styles: [`
-        /* :host { display: inline-block; } */
-    `]
+    template: `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-app" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11 2H5a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z"/></svg>`,
+    styles: [``]
 })
 export class SvgIconComponent implements AfterViewInit {
 
     @Input() public path!: string;
-    @Input() public size: string | undefined;
+    @Input() public size = '1rem';
     @Input() public iconClass: string | undefined;
 
     constructor(
@@ -22,9 +20,16 @@ export class SvgIconComponent implements AfterViewInit {
     ) { }
 
     public async ngAfterViewInit(): Promise<void> {
+        let svg = this.el.nativeElement.firstChild as SVGElement;
+        this.setIconProps(svg);
         const xml = await this.svg.loadSvgFile(this.path);
+        svg.remove();
         this.el.nativeElement.innerHTML = xml;
-        const svg = this.el.nativeElement.firstChild as SVGElement;
+        svg = this.el.nativeElement.firstChild as SVGElement;
+        this.setIconProps(svg);
+    }
+
+    private setIconProps(svg: SVGElement): void {
         if (!!this.size) {
             svg.setAttribute('width', this.size);
             svg.setAttribute('height', this.size);
