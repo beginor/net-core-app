a8cca059f13137b65b09c406c5bb44651ad8cdd4
beginor <beginor@qq.com>
2020-12-29 13:45:52 +0800

    timemaps

--- database/102_tile_maps.sql
+++ database/102_tile_maps.sql
@@ -1,8 +1,8 @@
--- table: public.tile_maps
+-- table: public.tilemaps
 
--- drop table public.tile_maps;
+-- drop table public.tilemaps;
 
-create table public.tile_maps
+create table public.tilemaps
 (
     id bigint not null default snow_flake_id(),
     name character varying(32) collate pg_catalog."default" not null,
@@ -15,12 +15,12 @@ create table public.tile_maps
     updater_id character varying(32) collate pg_catalog."default" not null,
     updated_at timestamp without time zone not null,
     is_deleted boolean not null,
-    constraint pk_tile_maps primary key (id),
-    constraint fk_tile_maps_creator foreign key (creator_id)
+    constraint pk_tilemaps primary key (id),
+    constraint fk_tilemaps_creator foreign key (creator_id)
         references public.app_users (id) match simple
         on update cascade
         on delete cascade,
-    constraint fk_tile_maps_updator foreign key (updater_id)
+    constraint fk_tilemaps_updator foreign key (updater_id)
         references public.app_users (id) match simple
         on update no action
         on delete no action
@@ -30,40 +30,40 @@ with (
 )
 tablespace pg_default;
 
-alter table public.tile_maps
+alter table public.tilemaps
     owner to postgres;
-comment on table public.tile_maps
+comment on table public.tilemaps
     is '切片地图';
 
-comment on column public.tile_maps.id
+comment on column public.tilemaps.id
     is '切片地图id';
 
-comment on column public.tile_maps.name
+comment on column public.tilemaps.name
     is '切片地图名称';
 
-comment on column public.tile_maps.cache_directory
+comment on column public.tilemaps.cache_directory
     is '缓存目录';
 
-comment on column public.tile_maps.map_tile_info_path
+comment on column public.tilemaps.map_tile_info_path
     is '切片信息路径';
 
-comment on column public.tile_maps.is_bundled
+comment on column public.tilemaps.is_bundled
     is '是否为紧凑格式';
 
-comment on column public.tile_maps.creator_id
+comment on column public.tilemaps.creator_id
     is '创建者id';
 
-comment on column public.tile_maps.created_at
+comment on column public.tilemaps.created_at
     is '创建时间';
 
-comment on column public.tile_maps.updater_id
+comment on column public.tilemaps.updater_id
     is '更新者id';
 
-comment on column public.tile_maps.updated_at
+comment on column public.tilemaps.updated_at
     is '更新时间';
 
-comment on column public.tile_maps.is_deleted
+comment on column public.tilemaps.is_deleted
     is '是否删除';
 
-comment on column public.tile_maps.content_type
+comment on column public.tilemaps.content_type
     is '内容类型';
