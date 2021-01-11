15df79c4822b64e10db4a6a209d8b034c634db8a
beginor <beginor@qq.com>
2021-01-11 15:21:54 +0800

    add datasourcereader interface.

---
+++ server/src/GisHub.DataServices/IDataSourceReader.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beginor.GisHub.DataServices.Models;
+
+namespace Beginor.GisHub.DataServices {
+
+    public interface IDataSourceReader {
+
+    }
+
+}
