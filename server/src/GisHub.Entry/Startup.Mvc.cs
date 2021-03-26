0a0531123e3bf493a5abf894ff102dd3e7356ffc
beginor <beginor@qq.com>
2021-03-26 16:24:13 +0800

    move to tilemap

--- server/src/GisHub.Entry/Startup.Mvc.cs
+++ server/src/GisHub.Entry/Startup.Mvc.cs
@@ -27,9 +27,6 @@ namespace Beginor.GisHub.Entry {
                     manager.ApplicationParts.Add(
                         new AssemblyPart(typeof(Beginor.GisHub.DataServices.ModelMapping).Assembly)
                     );
-                     manager.ApplicationParts.Add(
-                        new AssemblyPart(typeof(Beginor.GisHub.VectorTile.ModelMapping).Assembly)
-                    );
                 })
                 .AddControllersAsServices()
                 .ConfigureApiBehaviorOptions(options => {
