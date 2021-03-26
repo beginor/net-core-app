079df61e4d3c6a911d58a6c216fd8b0da6b07bc7
beginor <beginor@qq.com>
2021-03-26 15:45:22 +0800

    model mapping

--- server/src/GisHub.VectorTile/ModelMapping.cs
+++ server/src/GisHub.VectorTile/ModelMapping.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using Beginor.GisHub.VectorTile.Data;
+using Beginor.GisHub.VectorTile.Models;
 
 namespace Beginor.GisHub.VectorTile {
 
-    public class ModelMapping {
+    public class ModelMapping : AutoMapper.Profile {
+
+        public ModelMapping() {
+            CreateMap<Vectortile, VectortileModel>()
+                .ReverseMap()
+                .ForMember(dest => dest.Id, map => map.Ignore());
+        }
     }
 
 }
