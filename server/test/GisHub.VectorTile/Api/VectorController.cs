6df6969944dee45440ea2afe6f0eb010a5385550
beginor <beginor@qq.com>
2021-05-13 12:01:08 +0800

    refact to use single source.

--- server/test/GisHub.VectorTile/Api/VectorController.cs
+++ server/test/GisHub.VectorTile/Api/VectorController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Dapper;
 using Npgsql;
 
 namespace GisHub.VectorTile.Api {
@@ -25,14 +24,12 @@ namespace GisHub.VectorTile.Api {
             this.options = snapshot.Value;
         }
 
-        [HttpGet("{layer}/{z:int}/{y:int}/{x:int}")]
-        public async Task<ActionResult> Get(string layer, int z, int y, int x) {
-            if (!options.Layers.ContainsKey(layer)) {
-                return NotFound();
-            }
+        [HttpGet("{z:int}/{y:int}/{x:int}")]
+        public async Task<ActionResult> GetAll(int z, int y, int x) {
             try {
-                var buffer = await GetMvtAsync(layer, z, y, x);
-                return File(buffer, "application/vnd.mapbox-vector-tile");
+                var sql = BuildVectorSql(z, y, x);
+                var result = await GetMvtResultAsync(sql);
+                return result;
             }
             catch (Exception ex) {
                 logger.LogError(ex.Message);
@@ -40,31 +37,77 @@ namespace GisHub.VectorTile.Api {
             }
         }
 
-        private async Task<byte[]> GetMvtAsync(string layer, int z, int y, int x) {
+        private async Task<ActionResult> GetMvtResultAsync(string sql) {
+            if (string.IsNullOrEmpty(sql)) {
+                return NotFound();
+            }
+            var buffer = await GetMvtBufferAsync(sql);
+            if (buffer == null || buffer.Length == 0) {
+                return NotFound();
+            }
+            return File(buffer, "application/vnd.mapbox-vector-tile");
+        }
+
+        private async Task<byte[]> GetMvtBufferAsync(string sql) {
+            logger.LogInformation(sql);
+            await using var conn = new NpgsqlConnection(options.ConnectionString);
+            await conn.OpenAsync();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            var result = await cmd.ExecuteScalarAsync();
+            await conn.CloseAsync();
+            return result as byte[];
+        }
+
+        private string BuildVectorSqlForLayer(string layer, int z, int y, int x) {
             var vectorLayer = options.Layers[layer];
-            var sql = new StringBuilder();
-            sql.AppendLine("with mvt_geom as (");
-            sql.AppendLine("  select");
-            sql.AppendLine("    ST_AsMVTGeom(");
+            if (z < vectorLayer.Minzoom || z > vectorLayer.Maxzoom) {
+                return string.Empty;
+            }
+            var sqlBuilder = new StringBuilder();
+            sqlBuilder.AppendLine("with mvt_geom as (");
+            sqlBuilder.AppendLine("  select");
+            sqlBuilder.AppendLine("    ST_AsMVTGeom(");
             if (vectorLayer.Srid == 3857) {
-                sql.AppendLine($"      {vectorLayer.GeometryColumn},");
+                sqlBuilder.AppendLine($"      {vectorLayer.GeometryColumn},");
             }
             else {
-                sql.AppendLine($"      ST_Transform({vectorLayer.GeometryColumn}, 3857),");
+                sqlBuilder.AppendLine($"      ST_Transform({vectorLayer.GeometryColumn}, 3857),");
             }
-            sql.AppendLine($"      ST_TileEnvelope({z}, {x}, {y}),");
-            sql.AppendLine("      extent => 4096, buffer => 64");
-            sql.AppendLine($"    ) as {vectorLayer.GeometryColumn},");
-            sql.AppendLine($"    {vectorLayer.IdColumn}, {vectorLayer.AttributeColumns}");
-            sql.AppendLine($"  from {vectorLayer.Schema}.{vectorLayer.TableName}");
-            sql.AppendLine($"  where {vectorLayer.GeometryColumn} && ST_TileEnvelope({z}, {x}, {y}, margin => (64.0 / 4096))");
-            sql.AppendLine(")");
-            sql.AppendLine($"select ST_AsMVT(mvt_geom, '{layer}', 4096, '{vectorLayer.GeometryColumn}', '{vectorLayer.IdColumn}')");
-            sql.AppendLine("from mvt_geom;");
-            logger.LogInformation(sql.ToString());
-            await using var conn = new NpgsqlConnection(options.ConnectionString);
-            var buffer = await conn.ExecuteScalarAsync<byte[]>(sql.ToString());
-            return buffer;
+            sqlBuilder.AppendLine($"      ST_TileEnvelope({z}, {x}, {y}),");
+            sqlBuilder.AppendLine("      extent => 4096, buffer => 64");
+            sqlBuilder.AppendLine($"    ) as {vectorLayer.GeometryColumn},");
+            sqlBuilder.AppendLine($"    {vectorLayer.IdColumn}, {vectorLayer.AttributeColumns}");
+            sqlBuilder.AppendLine($"  from {vectorLayer.Schema}.{vectorLayer.TableName}");
+            sqlBuilder.AppendLine($"  where {vectorLayer.GeometryColumn} && ST_TileEnvelope({z}, {x}, {y}, margin => (64.0 / 4096))");
+            sqlBuilder.AppendLine(")");
+            sqlBuilder.AppendLine($"select ST_AsMVT(mvt_geom, '{layer}', 4096, '{vectorLayer.GeometryColumn}', '{vectorLayer.IdColumn}')");
+            sqlBuilder.AppendLine("from mvt_geom");
+            return sqlBuilder.ToString();
+        }
+
+        private string BuildVectorSql(int z, int y, int x) {
+            var sqls = new List<string>();
+            foreach (var pair in options.Layers) {
+                var sql = BuildVectorSqlForLayer(pair.Key, z, y, x);
+                if (!string.IsNullOrEmpty(sql)) {
+                    sqls.Add(sql);
+                }
+            }
+            if (sqls.Count == 0) {
+                return string.Empty;
+            }
+            var sqlBuilder = new StringBuilder();
+            sqlBuilder.AppendLine("select ((");
+            for (var i = 0; i < sqls.Count; i++) {
+                sqlBuilder.Append(sqls[i]);
+                if (i < sqls.Count - 1) {
+                    sqlBuilder.AppendLine(") || (");
+                }
+            }
+            sqlBuilder.AppendLine("))");
+            return sqlBuilder.ToString();
         }
 
     }
@@ -81,6 +124,8 @@ namespace GisHub.VectorTile.Api {
         public string AttributeColumns { get; set; }
         public string GeometryColumn { get; set; }
         public int Srid { get; set; }
+        public int Minzoom { get; set; } = 1;
+        public int Maxzoom { get; set; } = 19;
     }
 
 }
