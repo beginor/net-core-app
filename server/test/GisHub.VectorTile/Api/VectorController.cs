1f30196ea518d9866387ea012b0161b50ea380c8
beginor <beginor@qq.com>
2021-05-14 11:58:17 +0800

    refact vector tile.

--- server/test/GisHub.VectorTile/Api/VectorController.cs
+++ server/test/GisHub.VectorTile/Api/VectorController.cs
@@ -48,45 +48,6 @@ namespace GisHub.VectorTile.Api {
             return File(buffer, "application/vnd.mapbox-vector-tile");
         }
 
-        private async Task<byte[]> GetMvtBufferAsync(string sql) {
-            logger.LogInformation(sql);
-            await using var conn = new NpgsqlConnection(options.ConnectionString);
-            await conn.OpenAsync();
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = sql;
-            cmd.CommandType = CommandType.Text;
-            var result = await cmd.ExecuteScalarAsync();
-            await conn.CloseAsync();
-            return result as byte[];
-        }
-
-        private string BuildVectorSqlForLayer(string layer, int z, int y, int x) {
-            var vectorLayer = options.Layers[layer];
-            if (z < vectorLayer.Minzoom || z > vectorLayer.Maxzoom) {
-                return string.Empty;
-            }
-            var sqlBuilder = new StringBuilder();
-            sqlBuilder.AppendLine("with mvt_geom as (");
-            sqlBuilder.AppendLine("  select");
-            sqlBuilder.AppendLine("    ST_AsMVTGeom(");
-            if (vectorLayer.Srid == 3857) {
-                sqlBuilder.AppendLine($"      {vectorLayer.GeometryColumn},");
-            }
-            else {
-                sqlBuilder.AppendLine($"      ST_Transform({vectorLayer.GeometryColumn}, 3857),");
-            }
-            sqlBuilder.AppendLine($"      ST_TileEnvelope({z}, {x}, {y}),");
-            sqlBuilder.AppendLine("      extent => 4096, buffer => 64");
-            sqlBuilder.AppendLine($"    ) as {vectorLayer.GeometryColumn},");
-            sqlBuilder.AppendLine($"    {vectorLayer.IdColumn}, {vectorLayer.AttributeColumns}");
-            sqlBuilder.AppendLine($"  from {vectorLayer.Schema}.{vectorLayer.TableName}");
-            sqlBuilder.AppendLine($"  where {vectorLayer.GeometryColumn} && ST_TileEnvelope({z}, {x}, {y}, margin => (64.0 / 4096))");
-            sqlBuilder.AppendLine(")");
-            sqlBuilder.AppendLine($"select ST_AsMVT(mvt_geom, '{layer}', 4096, '{vectorLayer.GeometryColumn}', '{vectorLayer.IdColumn}')");
-            sqlBuilder.AppendLine("from mvt_geom");
-            return sqlBuilder.ToString();
-        }
-
         private string BuildVectorSql(int z, int y, int x) {
             var sqls = new List<string>();
             foreach (var pair in options.Layers) {
@@ -108,6 +69,45 @@ namespace GisHub.VectorTile.Api {
             }
             sqlBuilder.AppendLine("))");
             return sqlBuilder.ToString();
+        }
+
+        private string BuildVectorSqlForLayer(string layerName, int z, int y, int x) {
+            var layer = options.Layers[layerName];
+            if (z < layer.Minzoom || z > layer.Maxzoom) {
+                return string.Empty;
+            }
+            var sqlBuilder = new StringBuilder();
+            sqlBuilder.AppendLine("with mvt_geom as (");
+            sqlBuilder.AppendLine("  select");
+            sqlBuilder.AppendLine("    ST_AsMVTGeom(");
+            if (layer.Srid == 3857) {
+                sqlBuilder.AppendLine($"      {layer.GeometryColumn},");
+            }
+            else {
+                sqlBuilder.AppendLine($"      ST_Transform({layer.GeometryColumn}, 3857),");
+            }
+            sqlBuilder.AppendLine($"      ST_TileEnvelope({z}, {x}, {y}),");
+            sqlBuilder.AppendLine("      extent => 4096, buffer => 64");
+            sqlBuilder.AppendLine($"    ) as {layer.GeometryColumn},");
+            sqlBuilder.AppendLine($"    {layer.IdColumn}, {layer.AttributeColumns}");
+            sqlBuilder.AppendLine($"  from {layer.Schema}.{layer.TableName}");
+            sqlBuilder.AppendLine($"  where {layer.GeometryColumn} && ST_TileEnvelope({z}, {x}, {y}, margin => (64.0 / 4096))");
+            sqlBuilder.AppendLine(")");
+            sqlBuilder.AppendLine($"select ST_AsMVT(mvt_geom, '{layerName}', 4096, '{layer.GeometryColumn}', '{layer.IdColumn}')");
+            sqlBuilder.AppendLine("from mvt_geom");
+            return sqlBuilder.ToString();
+        }
+
+        private async Task<byte[]> GetMvtBufferAsync(string sql) {
+            logger.LogInformation(sql);
+            await using var conn = new NpgsqlConnection(options.ConnectionString);
+            await conn.OpenAsync();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            var result = await cmd.ExecuteScalarAsync();
+            await conn.CloseAsync();
+            return result as byte[];
         }
 
     }
