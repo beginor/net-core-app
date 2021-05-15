364642f5ede265b648b913677b8942c3e1919e9f
beginor <beginor@qq.com>
2021-05-15 15:50:44 +0800

    refact preview data template.

--- client/projects/web/src/app/data/datasources/preview/preview-data.component.ts
+++ client/projects/web/src/app/data/datasources/preview/preview-data.component.ts
@@ -8,15 +8,34 @@ import { DataSourceModel, DataSourceService, ReadDataParam } from '../datasource
     template: `
       <table class="table table-striped table-sm">
         <thead>
-          <tr>
-            <th scope="col" *ngFor="let col of columns" >
-              <div *ngIf="!!col.description" class="text-muted text-nowrap">{{col.description}}</div>
-              <div class="text-body text-nowrap">{{col.name}}</div>
-            </th>
-          </tr>
+          <ng-container *ngIf="loadingColumns;else colsTpl">
+            <tr>
+              <th class="text-center text-warning">正在加载字段列表...</th>
+            </tr>
+          </ng-container>
+          <ng-template #colsTpl>
+            <tr>
+              <th scope="col" *ngFor="let col of columns" >
+                <div *ngIf="!!col.description" class="text-muted text-nowrap">{{col.description}}</div>
+                <div class="text-body text-nowrap">{{col.name}}</div>
+              </th>
+            </tr>
+          </ng-template>
         </thead>
         <tbody>
-          <ng-container *ngIf="data.length > 0;else emptyTpl">
+          <ng-container *ngIf="loadingData;else dataTpl">
+            <tr>
+              <td class="text-warning text-center" [colSpan]="columns.length" >正在加载数据！</td>
+            </tr>
+          </ng-container>
+          <ng-template #dataTpl>
+            <ng-container *ngIf="isEmpty;else rowsTpl">
+              <tr>
+                <td class="text-warning text-center" [colSpan]="columns.length" >该数据源无数据！</td>
+              </tr>
+            </ng-container>
+          </ng-template>
+          <ng-template #rowsTpl>
             <tr *ngFor="let row of data; let i = index;">
               <td class="text-body text-nowrap" *ngFor="let col of columns">
               {{row[col.name]}}
@@ -26,11 +45,6 @@ import { DataSourceModel, DataSourceService, ReadDataParam } from '../datasource
               <td class="table-info" [colSpan]="columns.length">
               共 {{total}} 条记录， 预览仅显示前 {{data.length}} 条记录。
               </td>
-            </tr>
-          </ng-container>
-          <ng-template #emptyTpl>
-            <tr>
-              <td class="text-warning text-center" [colSpan]="columns.length" >该数据源无数据！</td>
             </tr>
           </ng-template>
         </tbody>
@@ -50,6 +64,9 @@ export class PreviewDataComponent implements OnInit, OnDestroy {
     public data: any[] = [];
     public total = 0;
     public readDataParam: ReadDataParam = { };
+    public loadingColumns = false;
+    public loadingData = false;
+    public isEmpty = false;
 
     constructor(
         private vm: DataSourceService
@@ -60,20 +77,35 @@ export class PreviewDataComponent implements OnInit, OnDestroy {
         if (!id) {
             return;
         }
-        let cols = await this.vm.getColumns(id);
-        if (!!this.ds.geometryColumn) {
-            cols = cols.filter(col => col.name !== this.ds.geometryColumn);
-        }
-        this.columns = cols;
-        this.readDataParam.$select = cols.map(col => col.name).join(',');
-        this.readDataParam.$take = 20;
-        const result = await this.vm.getData(id, this.readDataParam);
-        this.data = result.data ?? [];
-        this.total = result.total ?? 0;
+        await this.loadColumns();
+        await this.loadData();
     }
 
     public ngOnDestroy(): void {
         // alert('destroy');
+    }
+
+    private async loadColumns(): Promise<void> {
+        this.loadingColumns = true;
+        let cols = await this.vm.getColumns(this.ds.id);
+        if (!!this.ds.geometryColumn) {
+            cols = cols.filter(col => col.name !== this.ds.geometryColumn);
+        }
+        this.columns = cols;
+        this.loadingColumns = false;
+    }
+
+    private async loadData(): Promise<void> {
+        this.loadingData = true;
+        this.readDataParam.$select = this.columns.map(
+            col => col.name
+        ).join(',');
+        this.readDataParam.$take = 20;
+        const result = await this.vm.getData(this.ds.id, this.readDataParam);
+        this.data = result.data ?? [];
+        this.total = result.total ?? 0;
+        this.loadingData = false;
+        this.isEmpty = this.total === 0;
     }
 
 }
