99aef1018cdf6b0622e3290c66f3a9ca9946287b
beginor <beginor@qq.com>
2021-05-26 09:29:17 +0800

    add deploy single file script.

---
+++ docker/deploy-single-file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+if [ -z "$1" ]
+then
+  echo "Please provide depoly type: init, update"
+  exit 1
+fi
+
+if [ -z "$2" ]
+then
+  echo "Please provide deploy target: server:~/dotnet/"
+  exit 2
+fi
+
+if [ $1 == "update" ]
+then
+  rm -rf dist/config
+fi
+
+scp -r dist/* $2
