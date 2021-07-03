c528881d59a669d727627cbc20205803b7144760
beginor <beginor@qq.com>
2021-07-03 18:18:43 +0800

    fix typo.

--- client/projects/web/src/main.ts
+++ client/projects/web/src/main.ts
@@ -1,7 +1,7 @@
+import { enableProdMode } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh-Hans';
 import zhEx from '@angular/common/locales/extra/zh-Hans';
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
