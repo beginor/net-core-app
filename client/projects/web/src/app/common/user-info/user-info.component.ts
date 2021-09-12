9e797f2c1832c6b74d02f3d3ae6a8ae46bc8d50c
beginor <beginor@qq.com>
2021-09-12 10:11:34 +0800

    loading indicator.

--- client/projects/web/src/app/common/user-info/user-info.component.ts
+++ client/projects/web/src/app/common/user-info/user-info.component.ts
@@ -13,6 +13,8 @@ export class UserInfoComponent implements OnInit {
 
     public user?: UserInfo;
     public dob?: NgbDate;
+    public loading = false;
+    public loadingMessage = '';
 
     constructor(
         public account: AccountService,
@@ -22,6 +24,8 @@ export class UserInfoComponent implements OnInit {
 
     public async ngOnInit(): Promise<void> {
         try {
+            this.loading = true;
+            this.loadingMessage = '正在加载用户信息 ...';
             const user = await this.account.getUser();
             this.user = user;
             this.dob = NgbDate.from(
@@ -33,14 +37,19 @@ export class UserInfoComponent implements OnInit {
                 { type: 'danger', message: '无法获取用户信息！' }
             );
         }
+        finally {
+            this.loading = false;
+        }
     }
 
     public async saveUser(): Promise<void> {
         if (!this.user) {
             return;
         }
-        this.user.dateOfBirth = this.formatter.format(this.dob as NgbDate);
         try {
+            this.loading = true;
+            this.loadingMessage = '正在更新用户信息 ...';
+            this.user.dateOfBirth = this.formatter.format(this.dob as NgbDate);
             var user = await this.account.updateUser(this.user);
             this.user = user;
         }
@@ -48,6 +57,9 @@ export class UserInfoComponent implements OnInit {
             this.ui.showAlert(
                 { type: 'danger', message: '无法更新用户信息！' }
             );
+        }
+        finally {
+            this.loading = false;
         }
     }
 
