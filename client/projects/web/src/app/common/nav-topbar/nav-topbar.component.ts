74642d519c809cbbe0b5e1ebca0bcbef978c3842
beginor <beginor@qq.com>
2021-09-13 14:40:51 +0800

    refact nav-topbar

--- client/projects/web/src/app/common/nav-topbar/nav-topbar.component.ts
+++ client/projects/web/src/app/common/nav-topbar/nav-topbar.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 import { AccountService } from 'app-shared';
 import { NavigationService } from '../services/navigation.service';
-import { AccountComponent } from '../account/account.component';
 
 @Component({
     selector: 'app-nav-topbar',
@@ -18,7 +15,6 @@ export class NavTopBarComponent {
 
     constructor(
         private router: Router,
-        private modal: NgbModal,
         public account: AccountService,
         public navigation: NavigationService
     ) { }
@@ -28,14 +24,6 @@ export class NavTopBarComponent {
         e.preventDefault();
         await this.account.logout();
         await this.router.navigateByUrl('/');
-    }
-
-    public async showAccountInfoModal(e: MouseEvent): Promise<void> {
-        e.preventDefault();
-        this.modal.open(
-            AccountComponent,
-            { size: 'lg', container: 'body', backdrop: 'static' }
-        );
     }
 
 }
