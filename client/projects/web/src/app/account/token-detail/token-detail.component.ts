f4aef187d1de472037117ce173fdd46c6f320270
beginor <beginor@qq.com>
2021-09-17 15:49:07 +0800

    add user-token components.

---
+++ client/projects/web/src/app/account/token-detail/token-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-detail',
+  templateUrl: './token-detail.component.html',
+  styleUrls: ['./token-detail.component.scss']
+})
+export class TokenDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/projects/web/src/app/account/token-list/token-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-list',
+  templateUrl: './token-list.component.html',
+  styleUrls: ['./token-list.component.scss']
+})
+export class TokenListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
