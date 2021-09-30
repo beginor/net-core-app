8aa93d4875dffab755da497d6d4f697bf60e993a
beginor <beginor@qq.com>
2021-09-30 15:28:21 +0800

    remove swagger.

--- server/test/GisHub.VectorTile/Startup.cs
+++ server/test/GisHub.VectorTile/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using GisHub.VectorTile.Data;
 using SysEnvironment = System.Environment;
 
@@ -32,17 +31,12 @@ namespace GisHub.VectorTile {
             services.AddSingleton(cacheOptions);
             services.AddCors();
             services.AddControllers();
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "VectorTile", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VectorTile v1"));
             }
 
             var pathbase = GetAppPathbase();
