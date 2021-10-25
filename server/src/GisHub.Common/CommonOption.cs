bcb66b69b14ac69ee0a96e205dbb58ec4b73e717
beginor <beginor@qq.com>
2021-10-25 15:04:54 +0800

    add output options.

--- server/src/GisHub.Common/CommonOption.cs
+++ server/src/GisHub.Common/CommonOption.cs
@@ -4,6 +4,7 @@ namespace Beginor.GisHub.Common {
 
     public class CommonOption {
         public CacheOption Cache { get; set; } = new();
+        public OutputOption Output { get; set; } = new();
     }
 
     public class CacheOption {
@@ -11,6 +12,15 @@ namespace Beginor.GisHub.Common {
         public string Directory { get; set; } = "app_cache";
         public TimeSpan MemoryExpiration { get; set; } = TimeSpan.FromHours(1);
         public TimeSpan FileExpiration { get; set; } = TimeSpan.FromDays(1);
+    }
+
+    public class OutputOption {
+        public bool Compress { get; set; } = false;
+        public CoordinateOption Coordinate { get; set; } = new();
+    }
+
+    public class CoordinateOption {
+        public int digits { get; set; } = 6;
     }
 
 }
