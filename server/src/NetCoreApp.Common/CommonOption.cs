e0c70e1c589d322f5cb83a7c4f4a71d0d4c23193
beginor <beginor@qq.com>
2021-10-25 13:54:35 +0800

    refact to property

--- server/src/NetCoreApp.Common/CommonOption.cs
+++ server/src/NetCoreApp.Common/CommonOption.cs
@@ -9,8 +9,8 @@ namespace Beginor.NetCoreApp.Common {
     public class CacheOption {
         public bool Enabled { get; set; } = false;
         public string Directory { get; set; } = "app_cache";
-        public TimeSpan MemoryExpiration = TimeSpan.FromHours(1);
-        public TimeSpan FileExpiration = TimeSpan.FromDays(1);
+        public TimeSpan MemoryExpiration { get; set; } = TimeSpan.FromHours(1);
+        public TimeSpan FileExpiration { get; set; } = TimeSpan.FromDays(1);
     }
 
 }
