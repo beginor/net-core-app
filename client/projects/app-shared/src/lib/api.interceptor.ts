5f3caf4ba16ee2dddcf3868dfb51b255a226015c
beginor <beginor@qq.com>
2021-12-08 16:11:17 +0800

    refact api interseptor

--- client/projects/app-shared/src/lib/api.interceptor.ts
+++ client/projects/app-shared/src/lib/api.interceptor.ts
@@ -17,19 +17,23 @@ export class ApiInterceptor implements HttpInterceptor {
         req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        if (req.url.startsWith(this.apiRoot)) {
+        if (req.url.startsWith(this.apiRoot) ||
+            req.url.startsWith(makeAbsoluteUrl(this.apiRoot))
+        ) { // eslint-disable-line max-len
             const setHeaders: { [key: string]: string } = {
                 'X-Requested-With': 'XMLHttpRequest'
             };
             if (!!this.account.token) {
                 this.account.addAuthTokenTo(setHeaders);
             }
-            req = req.clone({
-                // withCredentials: true
-                setHeaders
-            });
+            req = req.clone({setHeaders});
         }
         return next.handle(req);
     }
 
+}
+
+export function makeAbsoluteUrl(url: string): string {
+    const u = new URL(url, window.self.location.href);
+    return u.toString();
 }
