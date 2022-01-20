9b2582287db1e79640028291e32c4b5739f34e08
beginor <beginor@qq.com>
2022-01-20 09:21:57 +0800

    refact build & deploy scripts.

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 ./build-server.sh linux-x64 postgis
+rm -rf dist/config
+rm -rf dist/wwwroot
+rm -rf dist/log
 # Build docker image
 docker build --no-cache --rm -t beginor/net-core-app .
 rm -rf dist
