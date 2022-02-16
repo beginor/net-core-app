3be57bfcede0843e001bd72d4da57799604f695a
beginor <beginor@qq.com>
2022-02-16 15:13:47 +0800

    refact

--- server/test/GisHub.VectorTile/build-single-file.sh
+++ server/test/GisHub.VectorTile/build-single-file.sh
@@ -10,7 +10,6 @@ dotnet publish -r linux-x64 --self-contained \
   -p:PublishReadyToRun=false \
   -p:DebugType=None \
   -o bin/Publish/linux-x64
-
 cp run.sh bin/Publish/linux-x64
 
 dotnet publish -r win-x64 --self-contained \
@@ -21,7 +20,6 @@ dotnet publish -r win-x64 --self-contained \
   -p:PublishReadyToRun=false \
   -p:DebugType=None \
   -o bin/Publish/win-x64
-
 cp run.bat bin/Publish/win-x64
 
 dotnet publish -r osx-x64 --self-contained \
@@ -32,5 +30,4 @@ dotnet publish -r osx-x64 --self-contained \
   -p:PublishReadyToRun=false \
   -p:DebugType=None \
   -o bin/Publish/osx-x64
-
 cp run.sh bin/Publish/osx-x64
