fe8107284ee118a786bf6447de65d6779682be14
beginor <beginor@qq.com>
2022-03-04 16:13:45 +0800

    render doc as markdown.

--- server/src/GisHub.DynamicSql/Api/DataApiController.data.cs
+++ server/src/GisHub.DynamicSql/Api/DataApiController.data.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using Beginor.AppFx.Api;
+using Beginor.AppFx.Core;
 using Beginor.GisHub.Common;
 using Beginor.GisHub.DataServices.Models;
 using Beginor.GisHub.DynamicSql.Data;
@@ -83,6 +84,86 @@ namespace Beginor.GisHub.DynamicSql.Api {
         [Consumes("application/x-www-form-urlencoded")]
         public Task<ActionResult<DataServiceFieldModel>> GetColumnsByPost(long id) {
             return GetColumnsImpl(id);
+        }
+
+        /// <summary>获取数据API的说明文档</summary>
+        [HttpGet("{id:long}/doc")]
+        [Authorize("data_apis.read_doc")]
+        [RolesFilter(IdParameterName = "id", ProviderType = typeof(IDataApiRepository))]
+        public async Task<ActionResult> GetApiDocs(long id) {
+            try {
+                var api = await repository.GetByIdAsync(id);
+                if (api == null) {
+                    return NotFound($"DataApi {id} does not exists.");
+                }
+                var doc = new StringBuilder();
+                // title and description
+                doc.AppendLine($"# {api.Name} 接口文档");
+                doc.AppendLine();
+                doc.AppendLine(api.Description);
+                doc.AppendLine();
+                // url
+                doc.AppendLine("## 地址");
+                doc.AppendLine();
+                var jsonUrl = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/api/dataapis/{id}/data";
+                var geoJsonUrl = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/api/dataapis/{id}/geojson";
+                doc.AppendLine($"- JSON 数据 <{jsonUrl}>");
+                if (api.GeometryColumn.IsNotNullOrEmpty()) {
+                    doc.AppendLine($"- GeoJSON 数据 <{geoJsonUrl}>");
+                }
+                doc.AppendLine();
+                // columns
+                doc.AppendLine("## 输出字段");
+                doc.AppendLine();
+                doc.AppendLine("| 名称 | 类型 | 说明 |");
+                doc.AppendLine("| :-- | :-- | :-- |");
+                var eoColType = (DataServiceFieldModel f) => f.Name.EqualsOrdinalIgnoreCase(api.GeometryColumn) ? "空间坐标" : f.Type;
+                foreach (var col in api.Columns) {
+                    doc.AppendLine($"| {col.Name} | {eoColType(col)} | {col.Description} |");
+                }
+                doc.AppendLine();
+                // parameters
+                doc.AppendLine("## 参数");
+                doc.AppendLine();
+                doc.AppendLine("| 名称 | 类型 | 说明 | 是否必须 |");
+                doc.AppendLine("| :-- | :-- | :-- | :----- |");
+                doc.AppendLine("| $token | string | 访问凭证 | 是 |");
+                var yesOrNo = (bool required) =>  required ? "是" : "否";
+                foreach (var param in api.Parameters) {
+                    doc.AppendLine($"| {param.Name} | {param.Type} | {param.Description} | {yesOrNo(param.Required)} |");
+                }
+                doc.AppendLine();
+                // attentions
+                doc.AppendLine("> 注意问题：");
+                doc.AppendLine(">");
+                doc.AppendLine("> 1. 凭证参数需要向数据接口提供者申请。");
+                doc.AppendLine("> 2. 数据接口暂时只支持使用 HTTP GET 方法请求，因此参数必须以 QueryString 的形式传递。");
+                doc.AppendLine();
+                // sample
+                doc.AppendLine("## 请求示例");
+                doc.AppendLine();
+                doc.AppendLine("请求 JSON 格式数据");
+                doc.AppendLine();
+                doc.AppendLine("```http");
+                doc.AppendLine($"{jsonUrl}?$token=API_TOKEN&{api.Parameters[0].Name}=");
+                doc.AppendLine("Referer: http://localhost:3000");
+                doc.AppendLine("```");
+                doc.AppendLine();
+                if (api.GeometryColumn.IsNotNullOrEmpty()) {
+                    doc.AppendLine("请求 GeoJSON 格式数据");
+                    doc.AppendLine();
+                    doc.AppendLine("```http");
+                    doc.AppendLine($"{geoJsonUrl}?$token=API_TOKEN&{api.Parameters[0].Name}=");
+                    doc.AppendLine("Referer: http://localhost:3000");
+                    doc.AppendLine("```");
+                }
+                // Response.Headers.ContentDisposition = "";
+                return Ok(doc.ToString());
+            }
+            catch (Exception ex) {
+                logger.LogError(ex, $"Can not get api doc for api {id} .");
+                return this.InternalServerError(ex);
+            }
         }
 
         private async Task<ActionResult<DataServiceFieldModel>> GetColumnsImpl(long id) {
