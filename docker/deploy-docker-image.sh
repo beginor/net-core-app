a4ed37cedde34088ccc7993449b7079390c5a6ad
beginor <beginor@qq.com>
2022-03-06 17:55:07 +0800

    refact client build script

--- docker/deploy-docker-image.sh
+++ docker/deploy-docker-image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 # push images to registry
-docker tag beginor/gishub 127.0.0.1:5000/beginor/gishub \
-  && docker push 127.0.0.1:5000/beginor/gishub \
-  && docker rmi 127.0.0.1:5000/beginor/gishub \
-  && docker tag beginor/gishub 127.0.0.1:5000/beginor/gishub:$(date +%Y%m%d) \
-  && docker push 127.0.0.1:5000/beginor/gishub:$(date +%Y%m%d) \
-  && docker rmi 127.0.0.1:5000/beginor/gishub:$(date +%Y%m%d)
+docker tag beginor/gishub 81.71.93.249:5000/beginor/gishub \
+  && docker push 81.71.93.249:5000/beginor/gishub \
+  && docker rmi 81.71.93.249:5000/beginor/gishub \
+  && docker tag beginor/gishub 81.71.93.249:5000/beginor/gishub:$(date +%Y%m%d) \
+  && docker push 81.71.93.249:5000/beginor/gishub:$(date +%Y%m%d) \
+  && docker rmi 81.71.93.249:5000/beginor/gishub:$(date +%Y%m%d)
 # Deploy to server
 ssh lighthouse -t '
 cd docker/gishub
