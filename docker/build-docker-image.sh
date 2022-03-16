b7f4587c7c6b2874159907487f06a04d65bdf6a9
beginor <beginor@qq.com>
2022-03-16 15:59:12 +0800

    use docker buildx to build image

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -2,7 +2,13 @@
 ./build-server.sh linux-x64 postgis
 rm -rf dist/config
 rm -rf dist/wwwroot
-rm -rf dist/log
+# rm -rf dist/log
+./build-client.sh
+
 # Build docker image
-docker build --no-cache --rm -t beginor/net-core-app .
+docker buildx build --pull --rm \
+  --platform linux/amd64 \
+  -t 192.168.1.13:5000/beginor/net-core-app \
+  --push .
+
 rm -rf dist
