55dc43fba79dfe64bb4bc7d9a780482dd1f8d006
beginor <beginor@qq.com>
2022-03-22 11:48:27 +0800

    add tests for Base64UrlEncoder

---
+++ server/test/NetCoreApp.Test/Common/Base64UrlEncoderTest.cs
@@ -0,0 +1,30 @@
+using Beginor.NetCoreApp.Common;
+using NUnit.Framework;
+
+namespace Beginor.NetCoreApp.Test.Common;
+
+[TestFixture]
+public class Base64UrlEncoderTest {
+
+    [Test]
+    public void _01_CanEncode() {
+        var input = "Hello,world!";
+        var result = Base64UrlEncoder.Encode(input);
+        Assert.AreEqual("SGVsbG8sd29ybGQh", result);
+        //
+        input += "!";
+        result = Base64UrlEncoder.Encode(input);
+        Assert.AreEqual("SGVsbG8sd29ybGQhIQ", result);
+    }
+
+    [Test]
+    public void _02_CanDecode() {
+        var input = "SGVsbG8sd29ybGQh";
+        var output = Base64UrlEncoder.Decode(input);
+        Assert.AreEqual("Hello,world!", output);
+        input = "SGVsbG8sd29ybGQhIQ";
+        output = Base64UrlEncoder.Decode(input);
+        Assert.AreEqual("Hello,world!!", output);
+    }
+
+}
