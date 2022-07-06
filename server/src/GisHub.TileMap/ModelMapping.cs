3dc44c5210d75f3a508d39e8a301e230d4be555c
beginor <beginor@qq.com>
2022-07-06 14:48:53 +0800

    fix model mapping for type

--- server/src/GisHub.TileMap/ModelMapping.cs
+++ server/src/GisHub.TileMap/ModelMapping.cs
@@ -1,17 +1,19 @@
 using Beginor.GisHub.TileMap.Data;
 using Beginor.GisHub.TileMap.Models;
 
-namespace Beginor.GisHub.TileMap; 
+namespace Beginor.GisHub.TileMap;
 
 public class ModelMapping : AutoMapper.Profile {
 
     public ModelMapping() {
         CreateMap<TileMapEntity, TileMapModel>()
             .ReverseMap()
-            .ForMember(dest => dest.Id, map => map.Ignore());
+            .ForMember(dest => dest.Id, map => map.Ignore())
+            .ForMember(dest => dest.Type, map => map.Ignore());
         CreateMap<VectorTileEntity, VectorTileModel>()
             .ReverseMap()
-            .ForMember(dest => dest.Id, map => map.Ignore());
+            .ForMember(dest => dest.Id, map => map.Ignore())
+            .ForMember(dest => dest.Type, map => map.Ignore());
     }
 
 }
