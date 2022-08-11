5d7bfaafc2b2113cb7b1f86c41c42e40f429ef62
beginor <beginor@qq.com>
2022-08-11 17:10:08 +0800

    format

--- server/test/NetCoreApp.Test/Data/AppLogRepositoryTest.cs
+++ server/test/NetCoreApp.Test/Data/AppLogRepositoryTest.cs
@@ -7,14 +7,18 @@ using NUnit.Framework;
 using Beginor.NetCoreApp.Data.Entities;
 using Beginor.NetCoreApp.Data.Repositories;
 using Beginor.NetCoreApp.Models;
+
 namespace Beginor.NetCoreApp.Test.Data;
+
 /// <summary>应用程序日志仓储测试</summary>
 [TestFixture]
 public class AppLogRepositoryTest : BaseTest<IAppLogRepository> {
+
     [Test]
     public void _01_CanResolveTarget() {
         Assert.IsNotNull(Target);
     }
+
     [Test]
     public async Task _02_CanDoSearchAsync() {
         var searchModel = new AppLogSearchModel {
