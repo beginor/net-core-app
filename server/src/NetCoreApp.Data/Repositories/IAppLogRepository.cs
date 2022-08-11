cffc875562295e3f537a8bfd85e024f07c3aa265
beginor <beginor@qq.com>
2022-08-11 17:16:48 +0800

    format

--- server/src/NetCoreApp.Data/Repositories/IAppLogRepository.cs
+++ server/src/NetCoreApp.Data/Repositories/IAppLogRepository.cs
@@ -3,7 +3,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Beginor.AppFx.Core;
 using Beginor.NetCoreApp.Models;
+
 namespace Beginor.NetCoreApp.Data.Repositories;
+
 /// <summary>应用程序日志 仓储接口</summary>
 public partial interface IAppLogRepository : IRepository<AppLogModel, long> {
     /// <summary>搜索 应用程序日志 ，返回分页结果。</summary>
