72457e50f9349dede37e2ae0428a613691558cb4
beginor <beginor@qq.com>
2023-12-07 11:44:05 +0800

    cleanup rename

--- rename.sh
+++ rename.sh
@@ -24,35 +24,22 @@ find . -name '*.bak' -delete
 # 提交一下 Docker 文件
 git add *
 git commit -m "Rename to ${COMPANY_NAME}.${PROJ_NAME}"
-# 修改客户端相关文件
-sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/package.json
-sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/angular.json
-sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/web/src/app/app.module.ts
-sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./client/projects/handset/src/app/app.module.ts
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./client/smartcode.yml
-sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./client/smartcode.yml
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./start.sh
-# 删除备份文件
-find . -name '*.bak' -delete
-# 提交一下客户端文件
-git add *
-git commit --amend -m "Rename to ${COMPANY_NAME}.${PROJ_NAME}"
 # 修改服务端相关文件
-sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./server/src/NetCoreApp.Entry/Properties/launchSettings.json
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Entry/Properties/launchSettings.json
+sed -i.bak "s/net-core-app/${CONTEXT_ROOT}/g" ./src/Entry/Properties/launchSettings.json
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./src/Entry/Properties/launchSettings.json
 grep Beginor.NetCoreApp -rl server --include *.cs | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
 grep Beginor.NetCoreApp -rl server --include *.hbm.xml | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
 grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
 grep Beginor.NetCoreApp -rl server --include *.csproj | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
 grep Beginor.NetCoreApp -rl server --include *.config | xargs sed -i.bak "s/Beginor\.NetCoreApp/${SERVER_PREFIX}/g"
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/src/NetCoreApp.Entry/Startup.Swagger.cs
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/test/NetCoreApp.Test/NetCoreApp.Test.csproj
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./src/Entry/Startup.Swagger.cs
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./test/Test/Test.csproj
 sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.gitlab-ci.yml
 sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/launch.json
 sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./.vscode/launch.json
 sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./.vscode/tasks.json
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./server/smartcode.yml
-sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./server/smartcode.yml
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ./smartcode.yml
+sed -i.bak "s/Beginor/${COMPANY_NAME}/g" ./smartcode.yml
 # 删除备份文件
 find . -name '*.bak' -delete
 # 提交一下服务端文件
@@ -62,24 +49,12 @@ git add -f ./.vscode/launch.json
 git add -f ./.vscode/tasks.json
 git commit --amend -m "Rename to ${COMPANY_NAME}.${PROJ_NAME}"
 # 移动文件至新的目录
-git mv server/src/NetCoreApp.Entry/NetCoreApp.Entry.csproj server/src/NetCoreApp.Entry/${PROJ_NAME}.Entry.csproj
-git mv server/src/NetCoreApp.Entry server/src/${PROJ_NAME}.Entry
-git mv server/src/NetCoreApp.Api/NetCoreApp.Api.csproj server/src/NetCoreApp.Api/${PROJ_NAME}.Api.csproj
-git mv server/src/NetCoreApp.Api server/src/${PROJ_NAME}.Api
-git mv server/src/NetCoreApp.Data/NetCoreApp.Data.csproj server/src/NetCoreApp.Data/${PROJ_NAME}.Data.csproj
-git mv server/src/NetCoreApp.Data server/src/${PROJ_NAME}.Data
-git mv server/src/NetCoreApp.Models/NetCoreApp.Models.csproj server/src/NetCoreApp.Models/${PROJ_NAME}.Models.csproj
-git mv server/src/NetCoreApp.Models server/src/${PROJ_NAME}.Models
-git mv server/src/NetCoreApp.Common/NetCoreApp.Common.csproj server/src/NetCoreApp.Common/${PROJ_NAME}.Common.csproj
-git mv server/src/NetCoreApp.Common server/src/${PROJ_NAME}.Common
-git mv server/test/NetCoreApp.Test/NetCoreApp.Test.csproj server/test/NetCoreApp.Test/${PROJ_NAME}.Test.csproj
-git mv server/test/NetCoreApp.Test server/test/${PROJ_NAME}.Test
-git mv server/NetCoreApp.sln server/${PROJ_NAME}.sln
+git mv NetCoreApp.sln ${PROJ_NAME}.sln
 # 提交一下服务端文件
 git add *
 git commit --amend -m "Rename to ${COMPANY_NAME}.${PROJ_NAME}"
 # 修改项目引用路径
-sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" server/${PROJ_NAME}.sln
+sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g" ${PROJ_NAME}.sln
 grep NetCoreApp -rl server --include *.csproj | xargs sed -i.bak "s/NetCoreApp/${PROJ_NAME}/g"
 # 删除备份文件
 find . -name '*.bak' -delete
