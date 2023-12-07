56d514a609b525adb1b505fac9f204ddaf133bf6
beginor <beginor@qq.com>
2023-12-07 11:43:52 +0800

    remove unused file

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-if ! [ -x "$(command -v concurrently)" ]; then
-  echo 'Warning: concurrently is not installed, install it with npm now .' >&2
-  npm i -g concurrently
-fi
-
-concurrently "cd client && npm run start" \
-  "cd server/src/NetCoreApp.Entry && dotnet watch run"
