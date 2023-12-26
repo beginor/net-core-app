bb46f1e71bf1020e0387e18e074d7d402a53b9c1
beginor <beginor@qq.com>
2023-12-26 11:22:53 +0800

    cleanup

--- src/Data/Repositories/AppOrganizeUnitRepository.cs
+++ src/Data/Repositories/AppOrganizeUnitRepository.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using AutoMapper;
 using Dapper;
 using NHibernate;
