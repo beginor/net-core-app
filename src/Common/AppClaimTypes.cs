4e3a45958660313ebbf459d1e8c9fc426901daf3
beginor <beginor@qq.com>
2024-01-08 16:03:38 +0800

    format

--- src/Common/AppClaimTypes.cs
+++ src/Common/AppClaimTypes.cs
@@ -1,13 +1,8 @@
 namespace Beginor.NetCoreApp.Common;
 
 public static class AppClaimTypes {
-
     public static readonly string Privilege = "AppPrivilege";
-
     public static readonly string OrganizeUnitId = "AppOrganizeUnitId";
-
     public static readonly string OrganizeUnitCode = "AppOrganizeUnitCode";
-
     public static readonly string AppUserId = "AppUserId";
-
 }
