e22076767e49850c0898525bb002a930305a893c
beginor <beginor@qq.com>
2024-07-22 14:24:44 +0800

    update build script

--- docker/build-client.sh
+++ docker/build-client.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -e
 # Build client project;
-cd ../client
+cd ../../net-core-app-client
 rm -rf dist
-pnpm run build-shared
-pnpm run build-web
-pnpm run build-handset
-cd ../docker
+pnpm build app-shared
+pnpm build web
+pnpm build handset
+cd ../net-core-app/docker
 # Check wwwroot folder
-if [ ! -d dist/wwwroot ]
+if [ -d dist/wwwroot ]
 then
-  mkdir -p dist/wwwroot
+  rm -rf mkdir -p dist/wwwroot
 fi
+mkdir -p dist/wwwroot
 # Copy client files to wwwroot
-cp -r ../client/dist/web dist/wwwroot
-cp -r ../client/dist/handset dist/wwwroot
+cp -r ../../net-core-app-client/dist/web/browser dist/wwwroot/web
+cp -r ../../net-core-app-client/dist/handset/browser dist/wwwroot/handset
 # Gzip static files (*.js, *.css).
 find dist -name "*.js" -print0 | xargs -0 gzip -k
 find dist -name "*.css" -print0 | xargs -0 gzip -k
