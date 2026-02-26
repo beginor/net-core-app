387671b5e7327ab929f2d3e21ee2d66a377a0ff2
beginor <beginor@qq.com>
2026-02-26 14:17:06 +0800

    fix docker build

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,17 +1,42 @@
 #!/bin/bash -e
-./build-server.sh linux-x64 postgis
-rm -rf dist/wwwroot
-rm -rf dist/config
-rm -rf dist/cache
-rm -rf dist/storage
-rm -rf dist/log
 
-./build-client.sh
+IMAGE="beginor/net-core-app"
 
-# Build docker image
-docker buildx build --pull --rm \
-  --platform linux/amd64 \
-  -t beginor/net-core-app \
-  --output type=image .
+build_docker_image() {
+    if [ -z "$1" ]; then
+        echo "Please provide target platform: linux, win or osx ."
+        exit 1
+    fi
+    if [ -z "$2" ]; then
+        echo "Please provide target arch: x64 or arm64 ."
+        exit 1
+    fi
 
-rm -rf dist
+    local platform="$1"
+    local arch="$2"
+    local runtime_id="$platform-$arch"
+
+    ./build-server.sh $runtime_id postgis
+    rm -rf dist/wwwroot
+    rm -rf dist/config
+    rm -rf dist/cache
+    rm -rf dist/storage
+    rm -rf dist/log
+
+    ./build-client.sh
+
+    # Build docker image
+    local tag="$2"
+    if [ "$arch" == "x64" ]; then
+        tag="amd64"
+    fi
+    docker build \
+      --platform $platform/$tag \
+      -t $IMAGE:$tag \
+      --output type=image .
+    rm -rf dist
+}
+
+build_docker_image "linux" "x64"
+build_docker_image "linux" "arm64"
+
--- docker/deploy-docker-image.sh
+++ docker/deploy-docker-image.sh
@@ -1,16 +1,35 @@
 #!/bin/bash -e
 
 REGISTRY="127.0.0.1:5000"
+
 IMAGE="beginor/net-core-app"
+IMAGE_AMD64="$IMAGE:amd64"
+IMAGE_ARM64="$IMAGE:arm64"
+
 TAG=$(date +"%Y%m%d%H%M%S")
 
 # push images to registry
-docker tag $IMAGE $REGISTRY/$IMAGE \
-  && docker push $REGISTRY/$IMAGE \
-  && docker rmi $REGISTRY/$IMAGE \
-  && docker tag $IMAGE $REGISTRY/$IMAGE:$TAG \
-  && docker push $REGISTRY/$IMAGE:$TAG \
-  && docker rmi $REGISTRY/$IMAGE:$TAG
+docker tag $IMAGE_AMD64 $REGISTRY/$IMAGE_AMD64
+docker tag $IMAGE_AMD64 $REGISTRY/$IMAGE_AMD64-$TAG
+docker push $REGISTRY/$IMAGE_AMD64-$TAG
+
+docker tag $IMAGE_ARM64 $REGISTRY/$IMAGE_ARM64
+docker tag $IMAGE_ARM64 $REGISTRY/$IMAGE_ARM64-$TAG
+docker push $REGISTRY/$IMAGE_ARM64-$TAG
+
+# create and push manifest
+docker manifest create --insecure $REGISTRY/$IMAGE:$TAG \
+  $REGISTRY/$IMAGE_ARM64-$TAG \
+  $REGISTRY/$IMAGE_AMD64-$TAG
+docker manifest push --insecure $REGISTRY/$IMAGE:$TAG
+
+# cleanup
+docker manifest rm $REGISTRY/$IMAGE:$TAG
+docker rmi $IMAGE_ARM64 $IMAGE_AMD64 \
+  $REGISTRY/$IMAGE_ARM64 $REGISTRY/$IMAGE_AMD64 \
+  $REGISTRY/$IMAGE_ARM64-$TAG $REGISTRY/$IMAGE_AMD64-$TAG
+
+echo "Final image is: $REGISTRY/$IMAGE:$TAG"
 
 # Deploy to server
 ssh ubuntu@127.0.0.1 -t '
